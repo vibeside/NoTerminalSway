fbb4c6dbfa94e7e13e35b283fb3506fe7b3f6e60
vibeside <vibeside01@gmail.com>
2024-02-14 14:57:26 -0500

    why the fuck didnt the plugin get included? wth?

---
+++ NoTerminalSway/NoSwayPlugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using MonoMod.Cil;
+using System;
+using System.Reflection;
+using UnityEngine.InputSystem;
+using MonoMod.RuntimeDetour;
+using GameNetcodeStuff;
+
+namespace NoTerminalSway
+{
+    [BepInPlugin(modGUID,modName,"9.2.8.0")]
+    public class NoSwayPlugin : BaseUnityPlugin
+    {
+        public const string modGUID = "grug.lethalcompany.NoTerminalSway";
+        public const string modName = "No Terminal Sway";
+        public void Awake()
+        {
+            Hook hudManagerHook = new(
+            typeof(PlayerControllerB).GetMethod(nameof(PlayerControllerB.Update), (BindingFlags)int.MaxValue),
+            (Action<PlayerControllerB> original, PlayerControllerB self) =>
+            {
+                if (self.inTerminalMenu)
+                {
+                    self.playerActions.Disable();
+                }
+                else
+                {
+                    self.playerActions.Enable();
+                }
+                original(self);
+            });
+        }
+    }
+}
